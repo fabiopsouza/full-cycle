d2c413b0714aa83123687077015ec35b1e0b0e4e
fabipereira <fabio.bpereira@mercadolivre.com>
2023-08-16 22:20:22 -0300

    Starting with graphql

--- 4-comunicacao-sistemas/graphql/cmd/server/server.go
+++ 4-comunicacao-sistemas/graphql/cmd/server/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"github.com/fabiopsouza/graphql/internal/database"
 	"log"
 	"net/http"
 	"os"
@@ -8,17 +10,28 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/fabiopsouza/graphql/graph"
+	_ "modernc.org/sqlite"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	db, err := sql.Open("sqlite", "./data.db")
+	if err != nil {
+		log.Fatalf("failed to open database: %v", err)
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategory(db)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDB,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
