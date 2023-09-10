a9a66bfac302b1a6af1432eeed7240e58cfe14c8
fabipereira <fabio.bpereira@mercadolivre.com>
2023-09-10 19:24:59 -0300

    Add mysql. Pending fix connection error and fix gitignore

--- 13-event-driven-architecture/fcutils/cmd/fcutils/main.go
+++ 13-event-driven-architecture/fcutils/cmd/fcutils/main.go
@@ -4,6 +4,13 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"github.com/devfullcycle/fcutils/internal/event"
+	"github.com/devfullcycle/fcutils/internal/usecase/create_account"
+	"github.com/devfullcycle/fcutils/internal/usecase/create_client"
+	"github.com/devfullcycle/fcutils/internal/usecase/create_transaction"
+	"github.com/devfullcycle/fcutils/internal/web"
+	"github.com/devfullcycle/fcutils/internal/web/webserver"
+	"github.com/devfullcycle/fcutils/pkg/events"
 
 	"github.com/devfullcycle/fcutils/internal/database"
 	"github.com/devfullcycle/fcutils/pkg/uow"
@@ -23,14 +30,14 @@ func main() {
 	//}
 	//kafkaProducer := kafka.NewKafkaProducer(&configMap)
 
-	//eventDispatcher := events.NewEventDispatcher()
+	eventDispatcher := events.NewEventDispatcher()
 	//eventDispatcher.Register("TransactionCreated", handler.NewTransactionCreatedKafkaHandler(kafkaProducer))
 	//eventDispatcher.Register("BalanceUpdated", handler.NewUpdateBalanceKafkaHandler(kafkaProducer))
-	//transactionCreatedEvent := event.NewTransactionCreated()
-	//balanceUpdatedEvent := event.NewBalanceUpdated()
-	//
-	//clientDb := database.NewClientDB(db)
-	//accountDb := database.NewAccountDB(db)
+	transactionCreatedEvent := event.NewTransactionCreated()
+	balanceUpdatedEvent := event.NewBalanceUpdated()
+
+	clientDb := database.NewClientDB(db)
+	accountDb := database.NewAccountDB(db)
 
 	ctx := context.Background()
 	uow := uow.NewUow(ctx, db)
@@ -42,20 +49,21 @@ func main() {
 	uow.Register("TransactionDB", func(tx *sql.Tx) interface{} {
 		return database.NewTransactionDB(db)
 	})
-	//createTransactionUseCase := create_transaction.NewCreateTransactionUseCase(uow, eventDispatcher, transactionCreatedEvent, balanceUpdatedEvent)
-	//createClientUseCase := create_client.NewCreateClientUseCase(clientDb)
-	//createAccountUseCase := createaccount.NewCreateAccountUseCase(accountDb, clientDb)
 
-	//webserver := webserver.NewWebServer(":8080")
-	//
-	//clientHandler := web.NewWebClientHandler(*createClientUseCase)
-	//accountHandler := web.NewWebAccountHandler(*createAccountUseCase)
-	//transactionHandler := web.NewWebTransactionHandler(*createTransactionUseCase)
-	//
-	//webserver.AddHandler("/clients", clientHandler.CreateClient)
-	//webserver.AddHandler("/accounts", accountHandler.CreateAccount)
-	//webserver.AddHandler("/transactions", transactionHandler.CreateTransaction)
+	createClientUseCase := create_client.NewCreateClientUseCase(clientDb)
+	createAccountUseCase := create_account.NewCreateAccountUseCase(accountDb, clientDb)
+	createTransactionUseCase := create_transaction.NewCreateTransactionUseCase(uow, eventDispatcher, transactionCreatedEvent, balanceUpdatedEvent)
+
+	webserver := webserver.NewWebServer(":3000")
+
+	clientHandler := web.NewWebClientHandler(*createClientUseCase)
+	accountHandler := web.NewWebAccountHandler(*createAccountUseCase)
+	transactionHandler := web.NewWebTransactionHandler(*createTransactionUseCase)
+
+	webserver.AddHandler("/clients", clientHandler.CreateClient)
+	webserver.AddHandler("/accounts", accountHandler.CreateAccount)
+	webserver.AddHandler("/transactions", transactionHandler.CreateTransaction)
 
 	fmt.Println("Server is running")
-	//webserver.Start()
+	webserver.Start()
 }
--- 13-event-driven-architecture/fcutils/go.mod
+++ 13-event-driven-architecture/fcutils/go.mod
@@ -4,6 +4,8 @@ go 1.20
 
 require (
 	github.com/confluentinc/confluent-kafka-go v1.9.2
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.0.10
 	github.com/go-sql-driver/mysql v1.7.1
 	github.com/google/uuid v1.3.1
 	github.com/mattn/go-sqlite3 v1.14.17
