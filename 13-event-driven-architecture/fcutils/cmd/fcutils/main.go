716e02212f2ca8d86c593e175f90f24c776bafb4
fabipereira <fabio.bpereira@mercadolivre.com>
2023-09-17 16:33:14 -0300

    Revert folder name

--- 13-event-driven-architecture/fcutils/cmd/fcutils/main.go
+++ 13-event-driven-architecture/fcutils/cmd/fcutils/main.go
@@ -21,7 +21,7 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "mysql", "3306", "wallet"))
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "mysql", "3306", "fcutils"))
 	if err != nil {
 		panic(err)
 	}
@@ -29,7 +29,7 @@ func main() {
 
 	configMap := ckafka.ConfigMap{
 		"bootstrap.servers": "kafka:29092",
-		"group.id":          "wallet",
+		"group.id":          "fcutils",
 	}
 	kafkaProducer := kafka.NewKafkaProducer(&configMap)
 
