152c7bb2eeb7a8adfa434a80af8535e43f4b72b7
fabipereira <fabio.bpereira@mercadolivre.com>
2023-09-17 16:29:19 -0300

    Change folder name

--- 13-event-driven-architecture/wallet/internal/event/transaction_created.go
+++ 13-event-driven-architecture/wallet/internal/event/transaction_created.go
@@ -3,28 +3,28 @@ package event
 import "time"
 
 type TransactionCreated struct {
-    Name string
-    Payload interface{}
+	Name    string
+	Payload interface{}
 }
 
 func NewTransactionCreated() *TransactionCreated {
-    return &TransactionCreated{
-        Name: "TransactionCreated",
-    }
+	return &TransactionCreated{
+		Name: "TransactionCreated",
+	}
 }
 
 func (e *TransactionCreated) GetName() string {
-    return e.Name
+	return e.Name
 }
 
 func (e *TransactionCreated) GetPayload() interface{} {
-    return e.Payload
+	return e.Payload
 }
 
 func (e *TransactionCreated) SetPayload(payload interface{}) {
-    e.Payload = payload
+	e.Payload = payload
 }
 
 func (e *TransactionCreated) GetDateTime() time.Time {
-    return time.Now()
+	return time.Now()
 }
