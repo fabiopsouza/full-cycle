0a7578489dfa3c474865aa2a8f187e4327908289
fabipereira <fabio.bpereira@mercadolivre.com>
2023-10-14 22:42:55 -0300

    Finished producer and consumer

--- 16-kafka/gokafka/cmd/producer/main.go
+++ 16-kafka/gokafka/cmd/producer/main.go
@@ -1,24 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"log"
 )
 
 func main() {
+	deliveryChan := make(chan kafka.Event)
 	producer := NewKafkaProducer()
 
-	err := Publish("mensagem", "teste", producer, nil)
+	err := Publish("transferiu", "teste", producer, []byte("transferencia"), deliveryChan)
 	if err != nil {
 		log.Println(err.Error())
 	}
 
-	producer.Flush(1000)
+	go DeliveryReport(deliveryChan)
+	producer.Flush(2000)
+
+	//e := <-deliveryChan
+	//msg := e.(*kafka.Message)
+	//if msg.TopicPartition.Error != nil {
+	//	fmt.Println("Erro ao enviar mensagem")
+	//} else {
+	//	fmt.Println("Mensagem enviada:", msg.TopicPartition)
+	//}
 }
 
 func NewKafkaProducer() *kafka.Producer {
 	configMap := &kafka.ConfigMap{
-		"bootstrap.servers": "gokafka-kafka-1:9092",
+		"bootstrap.servers":   "gokafka-kafka-1:9092",
+		"delivery.timeout.ms": "0",
+		"acks":                "all",
+		"enable.idempotence":  "true",
 	}
 
 	p, err := kafka.NewProducer(configMap)
@@ -29,17 +43,30 @@ func NewKafkaProducer() *kafka.Producer {
 	return p
 }
 
-func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
 	message := &kafka.Message{
 		Value:          []byte(msg),
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 		Key:            key,
 	}
 
-	err := producer.Produce(message, nil)
+	err := producer.Produce(message, deliveryChan)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func DeliveryReport(deliveryChan chan kafka.Event) {
+	for e := range deliveryChan {
+		switch ev := e.(type) {
+		case *kafka.Message:
+			if ev.TopicPartition.Error != nil {
+				fmt.Println("Erro ao enviar mensagem")
+			} else {
+				fmt.Println("Mensagem enviada:", ev.TopicPartition)
+			}
+		}
+	}
 }
